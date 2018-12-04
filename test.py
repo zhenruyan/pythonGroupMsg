79606dc88d1a5f8520dd4ee8053571f463057d31
zheruyan <admin@pkold.com>
2018-12-04 23:31:58 +0800

    test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     aatime = datetime.datetime.now()
     for c in range(1,3):
         e.append(c)
-    a = lib.GroupMessageUnSafe(profix="id:", idlist=e,loglevel=logging.DEBUG)
+    a = lib.GroupMessageUnSafe(profix="id:", idlist=e,loglevel=logging.INFO)
     a.initAllGroup()
     print("100000 queue init",(datetime.datetime.now() -aatime).microseconds/1000000,"s")
     bbtime = datetime.datetime.now()
@@ -23,3 +23,19 @@ if __name__ == '__main__':
         print(a.poll(660))
         print(a.poll(661))
         print(a.poll(662))
+    a.removeIdOfGroup("chat",660)
+    a.sendGroup("chat","helloworldsadasdasd")
+    a.sendGroup("chat","helloworldsadasdasdas")
+    for _ in range(0,2):
+        print(a.poll(660))
+        print(a.poll(661))
+        print(a.poll(662))
+    a.removeGroup("chat")
+    print(a.setGroup)
+    print(a.allQueue)
+    a.removeQueue(660)
+    a.removeQueue(1)
+    a.removeQueue(2)
+    print(a.allQueue)
+
+
